a8c92e5789bc0ec039d0515b9bd71cc92d89a95d
LaurentColpaert <yololoyololo2000@gmail.com>
2022-12-10 21:01:21 +0100

    simple 3d code from labs

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,4 @@
 #include<iostream>
-//for debug
-#include <fstream>
 
 //include glad before GLFW to avoid header conflict or define "#define GLFW_INCLUDE_NONE"
 #include <glad/glad.h>
@@ -10,16 +8,13 @@
 #include<glm/gtc/matrix_transform.hpp>
 #include<glm/gtc/type_ptr.hpp>
 
-
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
-#include "camera.h"
-#include "shader.h"
 
-#include <chrono>
-#include <thread>
-
+#include "./camera.h"
+#include "./shader.h"
+#include "./object.h"
 
 const int width = 500;
 const int height = 500;
@@ -27,8 +22,7 @@ const int height = 500;
 
 GLuint compileShader(std::string shaderCode, GLenum shaderType);
 GLuint compileProgram(GLuint vertexShader, GLuint fragmentShader);
-void processInput(GLFWwindow *window);
-
+void processInput(GLFWwindow* window);
 
 
 #ifndef NDEBUG
@@ -82,8 +76,17 @@ void APIENTRY glDebugOutput(GLenum source,
 
 Camera camera(glm::vec3(0.0, 0.0, 0.1));
 
+
 int main(int argc, char* argv[])
 {
+	std::cout << "Welcome to exercice 7: " << std::endl;
+	std::cout << "Complete light equation and attenuation factor\n"
+		"Implement the complete light equation.\n"
+		"Make the light move with time closer and further of the model.\n"
+		"Put the attenuation factor into the calculations\n"
+		"\n";
+
+
 	//Boilerplate
 	//Create the OpenGL context 
 	if (!glfwInit()) {
@@ -100,7 +103,7 @@ int main(int argc, char* argv[])
 
 
 	//Create the window
-	GLFWwindow* window = glfwCreateWindow(width, height, "main", nullptr, nullptr);
+	GLFWwindow* window = glfwCreateWindow(width, height, "Exercise 07", nullptr, nullptr);
 	if (window == NULL)
 	{
 		glfwTerminate();
@@ -108,11 +111,14 @@ int main(int argc, char* argv[])
 	}
 
 	glfwMakeContextCurrent(window);
+	
 	//load openGL function
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
 		throw std::runtime_error("Failed to initialize GLAD");
 	}
+
+	glEnable(GL_DEPTH_TEST);
 
 #ifndef NDEBUG
 	int flags;
@@ -128,95 +134,88 @@ int main(int argc, char* argv[])
 
 	const std::string sourceV = "#version 330 core\n"
 		"in vec3 position; \n"
-		"in vec2 texcoord; \n"
-		"out vec2 v_tex; \n"
-	
+		"in vec2 tex_coords; \n"
+		"in vec3 normal; \n"
+
+		"out vec3 v_frag_coord; \n"
+		"out vec3 v_normal; \n"
+
 		"uniform mat4 M; \n"
+		"uniform mat4 itM; \n"
 		"uniform mat4 V; \n"
 		"uniform mat4 P; \n"
+
+
 		" void main(){ \n"
-		"gl_Position = P*M*V*vec4(position, 1);\n"
-		"v_tex = texcoord; \n"
+		"vec4 frag_coord = M*vec4(position, 1.0); \n"
+		"gl_Position = P*V*frag_coord; \n"
+		
+		"v_normal = vec3(itM * vec4(normal, 1.0)); \n"
+		"v_frag_coord = frag_coord.xyz; \n"
+		"\n" 
+		"}\n";
 
-		"}\n"; 
 	const std::string sourceF = "#version 330 core\n"
-		"out vec4 FragColor;"
+		"out vec4 FragColor;\n"
 		"precision mediump float; \n"
-		"in vec2 v_tex; \n"
-		"uniform sampler2D ourTexture; \n"
+
+		"in vec3 v_frag_coord; \n"
+		"in vec3 v_normal; \n"
+
+		"uniform vec3 u_view_pos; \n"
+
+		//In GLSL you can use structures to better organize your code
+		//light
+		"struct Light{\n" 
+		"vec3 light_pos; \n"
+		"float ambient_strength; \n"
+		"float diffuse_strength; \n"
+		"float specular_strength; \n"
+		//attenuation factor
+		"float constant;\n"
+		"float linear;\n"
+		"float quadratic;\n"
+		"};\n"
+		"uniform Light light;"
+
+		"uniform float shininess; \n"
+		"uniform vec3 materialColour; \n"
+
+
+		"float specularCalculation(vec3 N, vec3 L, vec3 V ){ \n"
+			"vec3 R = reflect (-L,N);  \n " //reflect (-L,N) is  equivalent to //max (2 * dot(N,L) * N - L , 0.0) ;
+			"float cosTheta = dot(R , V); \n"
+			"float spec = pow(max(cosTheta,0.0), 32.0); \n"
+			"return light.specular_strength * spec;\n"
+		"}\n"
+
 		"void main() { \n"
-		"FragColor = texture(ourTexture, v_tex); \n"
+		"vec3 N = normalize(v_normal);\n"
+		"vec3 L = normalize(light.light_pos - v_frag_coord) ; \n"
+		"vec3 V = normalize(u_view_pos - v_frag_coord); \n"
+		"float specular = specularCalculation( N, L, V); \n"
+		"float diffuse = light.diffuse_strength * max(dot(N,L),0.0);\n"
+		"float distance = length(light.light_pos - v_frag_coord);"
+		"float attenuation = 1 / (light.constant + light.linear * distance + light.quadratic * distance * distance);"
+		"float light = light.ambient_strength + attenuation * (diffuse + specular); \n"
+		"FragColor = vec4(materialColour * vec3(light), 1.0); \n"
 		"} \n";
+	
+	Shader shader(sourceV, sourceF);
 
-	//1. With the shader class defined , you can either define string for your shader in the code
-	//Shader shader(sourceV, sourceF);
-
-	//Or you can have your shader written in text files
-	char fileVert[128] = "../../src/vertSrc.txt";
-	char fileFrag[128] = "../../src/fragSrc.txt";
-	Shader shader(fileVert, fileFrag);
-
-	// First object!
-	const float positionsData[15] = {
-		// vertices		  // texture coords
-		-1.0, -1.0, 0.0,	0.0, 0.0,
-		 1.0, -1.0, 0.0,	1.0, 0.0,
-		 0.0,  1.0, 0.0,	0.5, 1.0,
-	};
-
-
-	GLuint texture;
-	glGenTextures(1, &texture);
-	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_2D, texture);
 	
 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	char path[] = PATH_TO_OBJECTS "/sphere_smooth.obj";
+
+	Object sphere1(path);
+	sphere1.makeObject(shader, false);
+
+	char plane_path[] = PATH_TO_OBJECTS "/plane.obj";
+
+	Object plane(plane_path);
+	plane.makeObject(shader, false);
 
 	
-	stbi_set_flip_vertically_on_load(true);
-	int imWidth, imHeight, imNrChannels;
-	char file[128] = "../../src/assets/container.jpg";
-	unsigned char* data = stbi_load(file, &imWidth, &imHeight, &imNrChannels, 0);
-	if (data)
-	{
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, imWidth, imHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-		glGenerateMipmap(GL_TEXTURE_2D);
-	}
-	else {
-		std::cout << "Failed to Load texture" << std::endl;
-		const char* reason = stbi_failure_reason();
-		std::cout << reason << std::endl;
-	}
-
-	stbi_image_free(data);
-
-	//Create the buffer
-	GLuint VBO, VAO;
-	//generate the buffer and the vertex array
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-
-	//define VBO and VAO as active buffer and active vertex array
-	glBindVertexArray(VAO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(positionsData), positionsData, GL_STATIC_DRAW);
-
-	auto attribute = glGetAttribLocation(shader.ID, "position");
-	glEnableVertexAttribArray(attribute);
-	glVertexAttribPointer(attribute, 3, GL_FLOAT, false, 5 * sizeof(float), (void*)0);
-
-	auto att_tex = glGetAttribLocation(shader.ID, "texcoord");
-	glEnableVertexAttribArray(att_tex);
-	glVertexAttribPointer(att_tex, 2, GL_FLOAT, false, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-
-	//desactive the buffer
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
-
 
 	double prev = 0;
 	int deltaFrame = 0;
@@ -226,7 +225,7 @@ int main(int argc, char* argv[])
 		deltaFrame++;
 		if (deltaTime > 0.5) {
 			prev = now;
-			const double fpsCount = (double) deltaFrame / deltaTime;
+			const double fpsCount = (double)deltaFrame / deltaTime;
 			deltaFrame = 0;
 			std::cout << "\r FPS: " << fpsCount;
 			std::cout.flush();
@@ -234,17 +233,34 @@ int main(int argc, char* argv[])
 	};
 
 
+	glm::vec3 light_pos = glm::vec3(1.0, 2.0, 1.5);
 	glm::mat4 model = glm::mat4(1.0);
-	model = glm::translate(model,glm::vec3(0.5,0.5,-1.0));
-	model = glm::scale(model, glm::vec3(0.5,0.5,1.0));
+	model = glm::translate(model, glm::vec3(0.0, 0.0, -2.0));
+	model = glm::scale(model, glm::vec3(0.5, 0.5, 0.5));
 
+	glm::mat4 inverseModel = glm::transpose( glm::inverse(model));
 
 	glm::mat4 view = camera.GetViewMatrix();
-	glm::mat4 perspective = glm::perspective(45.0, 500.0/500.0, 0.01, 100.0);
+	glm::mat4 perspective = camera.GetProjectionMatrix();
 
-	//sync with the screen refresh rate
-	glfwSwapInterval(1);
+	float ambient = 0.1;
+	float diffuse = 0.5;
+	float specular = 0.8;
+
+	glm::vec3 materialColour = glm::vec3(0.2,0.6,0.8);
+
 	//Rendering
+
+	shader.use();
+	
+	shader.setFloat("shininess", 32.0f);
+	shader.setVector3f("materialColour", materialColour);
+	shader.setFloat("light.ambient_strength", ambient);
+	shader.setFloat("light.diffuse_strength", diffuse);
+	shader.setFloat("light.specular_strength", specular);
+	shader.setFloat("light.constant", 1.0);
+	shader.setFloat("light.linear", 0.14);
+	shader.setFloat("light.quadratic", 0.07);
 	
 	while (!glfwWindowShouldClose(window)) {
 		processInput(window);
@@ -252,24 +268,19 @@ int main(int argc, char* argv[])
 		glfwPollEvents();
 		double now = glfwGetTime();
 		glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
-		
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		glBindVertexArray(VAO);
-
-		//2. Use the shader Class to send the uniform
 		shader.use();
-
 		shader.setMatrix4("M", model);
+		shader.setMatrix4("itM", inverseModel);
 		shader.setMatrix4("V", view);
 		shader.setMatrix4("P", perspective);
-		
-		shader.setInteger("ourTexture", 0);
-		
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, texture);
-		
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		shader.setVector3f("u_view_pos", camera.Position);
+
+		auto delta = light_pos + glm::vec3(0.0,0.0,2 * std::sin(now));
+		shader.setVector3f("light.light_pos", delta);
+		sphere1.draw();
+		plane.draw();
 		
 		fps(now);
 		glfwSwapBuffers(window);
@@ -283,30 +294,32 @@ int main(int argc, char* argv[])
 }
 
 
-void processInput(GLFWwindow *window){
-	//3. Use the cameras class to change the parameters of the camera
+void processInput(GLFWwindow* window) {
+	
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
 
-	if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
-		camera.ProcessKeyboardMovement(UP, 1);
-	if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
-		camera.ProcessKeyboardMovement(DOWN, 1);
-
-	if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
-		camera.ProcessKeyboardMovement(LEFT, 1);
-	if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
-		camera.ProcessKeyboardMovement(RIGHT, 1);
-
-	if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
-		camera.ProcessKeyboardMovement(FORWARD, 1);
 	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-		camera.ProcessKeyboardMovement(BACKWARD, 1);
+		camera.ProcessKeyboardMovement(LEFT, 0.1);
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		camera.ProcessKeyboardMovement(RIGHT, 0.1);
 
-	if (glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS)
-		camera.ProcessKeyboardRotation(-1, 0.0); 
-	if (glfwGetKey(window, GLFW_KEY_T) == GLFW_PRESS)
-		camera.ProcessKeyboardRotation(1, 0.0);
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		camera.ProcessKeyboardMovement(FORWARD, 0.1);
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		camera.ProcessKeyboardMovement(BACKWARD, 0.1);
 
-	
+	if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
+		camera.ProcessKeyboardRotation(1, 0.0, 1);
+	if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
+		camera.ProcessKeyboardRotation(-1, 0.0, 1);
+
+	if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+		camera.ProcessKeyboardRotation(0.0, 1.0, 1);
+	if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+		camera.ProcessKeyboardRotation(0.0, -1.0, 1);
+
+
 }
+
+
