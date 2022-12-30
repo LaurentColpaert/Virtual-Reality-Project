79ac70c1d11f68af739504bf5413742acd8280f0
LaurentColpaert <yololoyololo2000@gmail.com>
2022-12-30 22:39:49 +0200

    Adding commentary

---
+++ src/utils/transform.h
@@ -0,0 +1,70 @@
+/**
+* @brief This header file defines the Transform class. Based on the `jeffeverett/opengl-driving-scene` class
+*
+* @author Adela Surca & Laurent Colpaert
+*
+* @project OpenGL project
+*
+**/
+#ifndef TRANSFORM_H
+#define TRANSFORM_H
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/quaternion.hpp>
+
+//We enable experimental because it allows us to use the quaternions
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/euler_angles.hpp>
+
+#include <iostream>
+
+class Transform{
+public:
+    glm::vec3 scale;
+    glm::vec3 translation;
+    glm::quat rotation;
+
+    glm::mat4 model;
+
+    Transform(){
+        scale = glm::vec3(1);
+        translation = glm::vec3(0);
+        rotation = glm::vec3(0);
+        model = glm::mat4(1);
+    }
+
+    void setScale(const glm::vec3 &scale_vector){
+        scale = scale_vector;
+    }
+    
+    void setTranslation(const glm::vec3 &translation_vector){
+        translation = translation_vector;
+    }
+
+    void setRotation(const glm::quat &quaternion){
+        rotation = quaternion;
+    }
+
+    void setRotation(const glm::vec3 &euler_angles){
+        rotation = glm::toQuat(glm::orientate3(euler_angles));
+    }
+
+    glm::vec3 getWorldTranslation(){
+        return glm::vec3(model[3][0], model[3][1], model[3][2]);
+    }
+
+    void updateModelMatrix(glm::mat4 starting_matrix){
+        glm::mat4 mtx(1);
+        mtx = glm::scale(mtx, scale);
+        glm::mat4 rotation_matrix = glm::toMat4(rotation);
+        mtx = rotation_matrix * mtx;
+        mtx[3][0] += translation.x;
+        mtx[3][1] += translation.y;
+        mtx[3][2] += translation.z;
+        model = starting_matrix * mtx;
+    }
+};
+#endif
